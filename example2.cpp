30698ef49d09bb5cb1d059c05c62b55d02b9ae88
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-02-05 23:37:02 +0500

    display Toyota blue car value on console

---
+++ example2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+main()
+{
+    int matrix[5][5]={{10,7,12,10,4},
+                     {18,11,15,17,2},
+                     {23,19,12,16,14},
+                     {7,12,16,0,2},
+                     {3,5,6,2,1}};
+    cout<<"toyota blue cars is:"<<matrix[1][3]<<endl;
+}
